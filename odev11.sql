8dcb4db6ff9cb565c479050d6f7c183efb4d0810
HaleGurpinar <106753721+HaleGurpinar@users.noreply.github.com>
2022-07-05 23:37:22 +0300

    Create odev11.sql

---
+++ odev11.sql
@@ -0,0 +1,6 @@
+--actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+
+
+actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+İlk 3 sorguyu tekrar eden veriler için de yapalım.
